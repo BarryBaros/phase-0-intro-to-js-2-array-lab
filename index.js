4db7fcdc5c1d9a6d0d6f618802565c8ccf81f2ab
Timothy Barasa <timbaros87@gmail.com>
2024-03-29 19:09:38 +0300

    Add destructive and nonedestructive methods

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const cats = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendCat(name) {
-    cats.push(name);
+  cats.push(name);
 }
+
 
     function destructivelyPrependCat(name) {
     cats.unshift(name);
     }
 
-      function destructivelyRemoveLastCat(name) {
-    cats.pop(name);
-}
+      function destructivelyRemoveLastCat() {
+        cats.pop();
+             }
+
+
    
-    function destructivelyRemoveFirstCat(name) {
-    cats.shift(name);
+    function destructivelyRemoveFirstCat() {
+    cats.shift();
 }
 
     function appendCat(name) {
